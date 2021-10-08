a0ebf681ad751d5c76f418b2730105926a56b1b2
Laura <62102447+laurapinos@users.noreply.github.com>
2021-10-08 20:06:08 -0300

    Update Chinelo.java

--- Loja de sapato OO/src/modelo/Chinelo.java
+++ Loja de sapato OO/src/modelo/Chinelo.java
@@ -1,8 +1,12 @@
 package modelo;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
+/**
+ * Classe responsável por instanciar objetos do tipo Chinelo
+ * @author Matheus Costa
+ *
+ */
 public class Chinelo extends Sapato{
 	private String estilo;
 	
@@ -17,120 +21,117 @@ public class Chinelo extends Sapato{
 		this.cor = c;
 		estilo = e;
 	}
-
+	
+	/**
+	 * @deprecated
+	 */
 	@Override
 	public String toString() {
 		return "-Marca: " + marca + "\n-Preço R$ " + preco + "\n-Quantidade em estoque: "
 				+ quantidade  + "\n-Cor: " + cor + "\n-Estilo da tira: " + estilo + "\n";
 	}
 	
-	public void preCadastrosChinelo() {
-		Chinelo chinelo1 = new Chinelo("Chinelo Rider", 39.90, 10, "Preto", "Normal");
-		
-		listaDeChinelo.add(chinelo1);
-	}
-	
-	
+	/**
+	 * Variável do tipo ArrayList que vai armazenar todos os objetos do tipo Chinelo
+	 */
 	ArrayList<Chinelo> listaDeChinelo = new ArrayList<Chinelo>();
-	Scanner ler = new Scanner (System.in);
 	
-	
-	public void cadastrar() {
-		String marca, cor, estilo;
-		double preco;
-		int qtd;
+	/**
+	 * Cria objetos do tipo Chinelo que vão servir
+	 * como dados cadastrados iniciais, e os adiciona no ArrayList
+	 */
+	public void preCadastrosChinelo() {
+		Chinelo chinelo1 = new Chinelo("Chinelo Nike",159.90, 50, "Preto","slide");
+		Chinelo chinelo2 = new Chinelo("Chinelo Colcci", 79.90, 50, "Preto e vermelho","normal");
+		Chinelo chinelo3 = new Chinelo("Chinelo Reserva",64.99, 50, "branco","normal");
+		Chinelo chinelo4 = new Chinelo("Chinelo Vans", 179.99, 50, "Xadrez","slide");
 
-		System.out.println("-> CADASTRO DE CHINELO");
-		System.out.print("Marca: ");
-		marca = ler.nextLine();
-		System.out.print("Preço R$ ");
-		preco = ler.nextDouble();
-		System.out.print("Quantidade a ser cadastrada: ");
-		qtd = ler.nextInt();
-		ler.nextLine();
-		System.out.print("Cor: ");
-		cor = ler.nextLine();
-		System.out.print("Estilo da tira: ");
-		estilo = ler.nextLine();
-		
-		Chinelo chinelo = new Chinelo(marca, preco, qtd, cor, estilo);
+		listaDeChinelo.add(chinelo1);
+		listaDeChinelo.add(chinelo2);
+		listaDeChinelo.add(chinelo3);
+		listaDeChinelo.add(chinelo4);
+	}
+	public Chinelo retornaChinelo(int pos) {
+		return listaDeChinelo.get(pos);		
+	}
+	
+	
+	/**
+	 * Adiciona um novo objeto Chinelo no ArrayList
+	 * @param chinelo : objeto que será adicionado no ArrayList
+	 */
+	public void cadastrar(Chinelo chinelo) {
 		listaDeChinelo.add(chinelo);
-		
-	}
-
-	public void visualizar() {
-		int modo;
-		
-		if(listaDeChinelo.size() == 0) System.out.print("\nNão há nenhum chinelo cadastrado.\n");
-		
-		else {
-			do {
-				boolean check = false;
-				System.out.println("-> VISUALIZAÇÃO DE CHINELO");
-				System.out.println("\nDeseja visualizar por:"
-						+ "\n1- Faixa de preço"
-						+ "\n2- Marca"
-						+ "\n3- Visualizar todos"
-						+ "\n4- Sair da visualização");
-				System.out.print(">> ");
-				modo = ler.nextInt();
-				ler.nextLine();
-				
-				
-				if(modo == 1) {
-					int precoMin, precoMax;
-					System.out.print("\nInforme o preço mínimo (valor inteiro): ");
-					precoMin = ler.nextInt();
-					System.out.print("Informe o preço máximo (valor inteiro): ");
-					precoMax = ler.nextInt();
-					
-					for(int i=0; i<listaDeChinelo.size(); i++) {
-						if(precoMin < listaDeChinelo.get(i).getPreco() && listaDeChinelo.get(i).getPreco() < precoMax){
-							check = true;
-							System.out.println("\n-> PRODUTO " + (i+1));
-							System.out.println(listaDeChinelo.get(i).toString());
-						}
-					}
-					
-					if(!check) {
-						System.out.println("Não há nenhum produto nessa faixa de preço!\n");
-					}
-				}
-				else if(modo == 2) {
-					String marca;
-					System.out.print("\nInforme o nome da marca: ");
-					marca = ler.nextLine();
-					
-					
-					for(int i=0; i<listaDeChinelo.size(); i++) {
-						if(marca.equals(listaDeChinelo.get(i).getMarca())) {
-							check = true;
-							System.out.println("\n-> PRODUTO " + (i+1));
-							System.out.println(listaDeChinelo.get(i).toString());
-						}
-					}
-					if(!check) {
-						System.out.println("Marca não encontrada!");
-					}
-				}
-				else if(modo == 3) {
-					for(int i=0; i<listaDeChinelo.size(); i++) {
-						System.out.println("\n-> PRODUTO " + (i+1));
-						System.out.println(listaDeChinelo.get(i).toString());
-					}
-				}
-				else if(modo == 4) {
-					System.out.println("\nEncerrando visualização...");
-					break;
-				}
-				else {
-					System.out.println("Opção inválida!");
-				}
-				
-			}while(modo != 4);	
-		}
 	}
 	
+	/**
+	 * Busca pelo valor de um atributo específico de um
+	 * objeto Chinelo, dentro do ArrayList
+	 * @param pos : é a posição do objeto selecionado dentro do ArrayList
+	 * @param info : é o parametro que indica qual valor será buscado
+	 * @return uma variável String que recebe o valor buscado
+	 */
+	public String retornaDado(int pos, int info) {
+		String dado = "";
+		
+		if(info == 1) {
+			dado = listaDeChinelo.get(pos).getMarca();
+		}
+		else if(info == 2) {
+			dado = String.valueOf(listaDeChinelo.get(pos).getPreco());
+		}
+		else if(info == 3) {
+			dado = String.valueOf(listaDeChinelo.get(pos).getQuantidade());
+		}
+		else if(info == 4) {
+			dado = listaDeChinelo.get(pos).getCor();
+		}
+		else if(info == 5) {
+			dado = listaDeChinelo.get(pos).getEstilo();
+		}
+		
+		return dado;
+	}
+	
+	/**
+	 * Altera o valor de algum atributo de um objeto Chinelo já existente
+	 * no ArrayList
+	 * @param pos : é a posição do objeto selecionado dentro do ArrayList
+	 * @param dado : o novo valor do atributo alterado
+	 * @param info : é o parametro que indica qual valor será alterado
+	 */
+	public void editar(int pos, String dado, int info) {
+		
+		if(info == 1) {
+			listaDeChinelo.get(pos).setMarca(dado);
+		}
+		else if(info == 2) {
+			listaDeChinelo.get(pos).setPreco(Double.parseDouble(dado));
+		}
+		else if(info == 3) {
+			listaDeChinelo.get(pos).setQuantidade(Integer.parseInt(dado));
+		}
+		else if(info == 4) {
+			listaDeChinelo.get(pos).setCor(dado);
+		}
+		else if(info == 5) {
+			listaDeChinelo.get(pos).setEstilo(dado);
+		}	
+	}
+	
+	/**
+	 * Remove um objeto Chinelo presente no ArrayList
+	 * @param n : é a posição em que o objeto a ser removido se encontra dentro do ArrayList
+	 */
+	public void deletar(int n) {
+		listaDeChinelo.remove(listaDeChinelo.get(n));
+	}
+	
+	/**
+	 * Busca pelo nome da marca de cada objeto Chinelo armazenado
+	 * no ArrayList e insere em um Array do tipo String
+	 * @return um Array que armazena os nomes das marcas dos chinelos
+	 */
 	public String[] listaNomesSapatos() {
 		String[] listaNomes = new String[200];
 		for(int i=0; i< listaDeChinelo.size(); i++) {
